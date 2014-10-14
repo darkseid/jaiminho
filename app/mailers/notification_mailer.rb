3a36a961990f670087f18ed544affb021396103d
Rafael Manoel <rafael.manoel@gmail.com>
2014-10-14 13:01:38 -0300

    testando a o preview de emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,15 +1,16 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "jaiminho@estantevirtual.com.br"
+  default content_type: "text/html"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notification_mailer.notify.subject
   #
-  def notify
-    data = {'name' => 'Rafael'}
-    template = ERB.new(NotificationTemplate.first.body)
+  def notify(data={}, template_name)
+    notification_template = NotificationTemplate.find_by_name(template_name)
+    template = ERB.new(notification_template.body)
     html = template.result(binding)
-    mail to: "to@example.org", body: html
+    mail(to: "rmanoel@estantevirtual.com.br", body: html).deliver!
   end
 end
--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -1,6 +1,6 @@
 class NotificationMailerPreview < ActionMailer::Preview
   # Accessible from http://localhost:3000/rails/mailers/notifier/welcome
   def welcome
-    NotificationMailer.notify
+    NotificationMailer.notify({name: 'Teste'}, 'nova_compra')
   end
 end
