d97966aeb6cdd5f057f968b300a8efa32afaf298
Raphael Cuba <raphael@raphaelcuba.com>
2014-10-16 17:33:33 -0300

    convertendo o campo body de varchar para text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013160104) do
+ActiveRecord::Schema.define(version: 20141016202819) do
 
   create_table "notification_templates", force: true do |t|
     t.string   "name"
