01fb1aa553321f3cab9045bcf19197ad1a56a37d
Raphael Cuba <raphael@raphaelcuba.com>
2014-10-16 18:01:37 -0300

    removendo fields from e to da classe notification_template

--- spec/models/template_notification_spec.rb
+++ spec/models/template_notification_spec.rb
@@ -5,7 +5,5 @@ RSpec.describe NotificationTemplate, :type => :model do
     it { should validate_uniqueness_of :name}
     it { should validate_presence_of :name}
     it { should validate_presence_of :body}
-    xit { should allow_value('email@domain.com').for(:to)}
-    xit { should_not allow_value('email').for(:to)}
   end
 end
