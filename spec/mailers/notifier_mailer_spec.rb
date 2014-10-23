371959b11e11cb8c2986e004fef8d6453f836201
Raphael Cuba <raphael@raphaelcuba.com>
2014-10-23 19:39:17 -0200

    code at styleguide

--- spec/mailers/notifier_mailer_spec.rb
+++ spec/mailers/notifier_mailer_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe NotifierMailer, :type => :mailer do
     context "email_to and template_name are required" do
 
       it "throws an ArgumentError if email_to is not informed" do
-        expect { NotifierMailer.notify(nil, 'template_name') }.to raise_error(ArgumentError)
+        expect { NotifierMailer.notify(nil, 'template_name') }.to raise_error ArgumentError
       end
 
       it "throws an ArgumentError if template_name is not informed" do
-        expect { NotifierMailer.notify('email@domain.com', nil) }.to raise_error(ArgumentError)
+        expect { NotifierMailer.notify('email@domain.com', nil) }.to raise_error ArgumentError
       end
 
       it "does not throw an ArgumentError if both are informed" do
-        expect { NotifierMailer.notify('email@domain.com', 'template_name') }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { NotifierMailer.notify('email@domain.com', 'template_name') }.to raise_error ActiveRecord::RecordNotFound
       end
 
     end
@@ -23,7 +23,7 @@ RSpec.describe NotifierMailer, :type => :mailer do
     context "sends email" do
 
       let!(:email) {
-        NotifierMailer.notify('email@test.com', 'template_name_test', {test: "Text"})
+        NotifierMailer.notify 'email@test.com', 'template_name_test', {test: "Text"}
       }
 
       before do
@@ -33,7 +33,7 @@ RSpec.describe NotifierMailer, :type => :mailer do
       it { expect(ActionMailer::Base.deliveries).not_to be_empty }
 
       it "renders the body" do
-        expect(email.body.to_s).to match("<h1>Text</h1>")
+        expect(email.body.to_s).to match "<h1>Text</h1>"
       end
 
     end
