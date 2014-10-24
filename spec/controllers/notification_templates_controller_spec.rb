0f86701ded129505a0caaf45501bd5a92dfa8fd0
Raphael Cuba <raphael@raphaelcuba.com>
2014-10-24 17:53:00 -0200

    created test for notification_controller

---
+++ spec/controllers/notification_templates_controller_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe NotificationTemplatesController, :type => :controller do
+
+  before :example do
+  end
+
+  describe "GET index" do
+
+    it "assigns @notification_templates" do
+      notification_templates = NotificationTemplate.all
+      get :index
+      expect(assigns :notification_templates).to eq notification_templates
+    end
+
+  end
+
+  describe "GET new" do
+    it "renders the index template" do
+      get :new
+      expect(assigns :notification_template).to be_a_new NotificationTemplate
+    end
+  end
+
+end
