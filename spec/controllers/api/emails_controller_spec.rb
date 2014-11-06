d76e4a440d2c1de0a112f12cfb9408b85b36e1de
Raphael Cuba <raphael@raphaelcuba.com>
2014-11-06 17:56:11 -0200

    removing tests

--- spec/controllers/api/emails_controller_spec.rb
+++ spec/controllers/api/emails_controller_spec.rb
@@ -2,25 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Api::EmailsController, :type => :controller do
 
-  describe "GET send" do
-
-    context "with valid params" do
-
-      it "returns http success" do
-        get :send
-        expect(response).to be_success
-      end
-
-      it ""
-      it ""
-      it ""
-      it ""
-      it ""
-      it ""
-      it ""
-
-    end
-
-  end
-
 end
