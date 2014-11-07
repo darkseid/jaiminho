e4dca66ffedf6aea65c90b34487f8eb12dd663a9
Rafael Santos <rafaeltravel88@gmail.com>
2014-11-07 18:15:53 -0200

    [WIP] Fixing email controller tests to work with API Module.

--- spec/controllers/api/emails_controller_spec.rb
+++ spec/controllers/api/emails_controller_spec.rb
@@ -1,50 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe Api::EmailsController, :type => :controller do
+RSpec.describe Api::EmailsController, type: :controller do
 
-  # describe "POST create" do
+  describe 'POST create' do
+    context 'with valid params' do
 
-  #   context "with valid params" do
-  #     let(:email_report) {
-  #       build(:email_report)
-  #     }
+      let(:email_report) { build(:email_report) }
 
-  #     before do
-  #       allow_any_instance_of(EmailsController).to receive(:create_email_report).and_return(email_report)
-  #       post :create, email_report: email_report.attributes, format: :json
-  #     end
+      before do
+        allow_any_instance_of(Api::EmailsController).to \
+          receive(:create_email_report).and_return(email_report)
+        post :create, email_report: email_report.attributes, format: :json
+      end
 
-  #     it "returns http success" do
-  #       expect(response).to be_success
-  #     end
+      it 'returns http success' do
+        expect(response).to be_success
+      end
 
-  #     it "save email report" do
-  #       expect(assigns(:email_report).attributes).to eql(email_report.attributes)
-  #     end
+      it 'save email report' do
+        expect(assigns(:email_report).attributes).to \
+          eql(email_report.attributes)
+      end
 
-  #     it "render create template" do
-  #       expect(response).to render_template :create
-  #     end
+      it 'render create template' do
+        expect(response).to render_template :create
+      end
+    end
 
-  #   end
+    context 'with invalid params' do
 
-  #   context "with invalid params" do
+      let(:email_report) { build(:invalid_email_report) }
 
-  #     let(:email_report) {
-  #       build(:invalid_email_report)
-  #     }
+      before do
+        allow_any_instance_of(Api::EmailsController).to \
+          receive(:create_email_report).and_return(email_report)
+        post :create, email_report: email_report.attributes, format: :json
+      end
 
-  #     before do
-  #       allow_any_instance_of(EmailsController).to receive(:create_email_report).and_return(email_report)
-  #       post :create, email_report: email_report.attributes, format: :json
-  #     end
+      it 'render template error' do
+        expect(response).to render_template :error
+      end
 
-  #     it "render template error" do
-  #       expect(response).to render_template :error
-  #     end
-
-  #   end
-
-  # end
-
+    end
+  end
 end
