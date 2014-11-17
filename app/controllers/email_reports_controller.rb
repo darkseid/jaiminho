8021a275955a3d0bc627d558b34d04b9f14e853d
Rafael Santos <rafaeltravel88@gmail.com>
2014-11-17 17:40:09 -0200

    Moves search logic from EmailReportController#index to EmaiLReport.get_by_status_and_email_template_id

--- app/controllers/email_reports_controller.rb
+++ app/controllers/email_reports_controller.rb
@@ -1,7 +1,10 @@
 class EmailReportsController < ApplicationController
   def index
-    @email_reports = EmailReport.where(filter_params.slice(:status,
-                                                     :email_template_id))
+    @email_reports = EmailReport.get_by_status_and_email_template_id(
+      status: filter_params[:status],
+      email_template_id: filter_params[:email_template_id]
+    )
+      
     @email_reports = @email_reports.sort_by { |email_report| email_report[:created_at] }.reverse!
     @email_templates = EmailTemplate.all
   end
--- app/models/email_report.rb
+++ app/models/email_report.rb
@@ -5,8 +5,19 @@ class EmailReport < ActiveRecord::Base
   validates_presence_of :email_to, :email_template, :email_template_id, :status
   enum status: [:sending, :success, :failed]
 
+  def self.get_by_status_and_email_template_id(status: nil, email_template_id: nil)
+    criteria = build_criteria(status: status, email_template_id: email_template_id)
+    where(criteria)
+  end
+
   def mark_as_successful
     @status = :success
     save
   end
+
+  private
+  def self.build_criteria(criterions={})
+    criterions.reject{|field, value| value.nil?} 
+  end
+
 end
