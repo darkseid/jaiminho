aab51b50ded9e723d6df33682dd1149b58644b98
Rafael Santos <rafaeltravel88@gmail.com>
2014-11-18 16:18:25 -0200

    Extract render methods logic from EmailTemplate to EmailTemplateRender

--- spec/factories/email_templates.rb
+++ spec/factories/email_templates.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
     body { Faker::Lorem.paragraph }
     subject { Faker::Lorem.sentence }
 
+    trait :with_placeholder do
+      subject "Oi <%= data['name'] %>"
+      body "Obrigado por comprar <%= data['product'] %>"
+    end
+
     factory :invalid_email_template do
       name nil
       body nil
