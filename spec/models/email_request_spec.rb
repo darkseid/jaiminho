f2531405b584f01a14a877459a4fd67222377a60
Rafael Santos <rafaeltravel88@gmail.com>
2014-11-19 18:31:47 -0200

    Refacts Email Request view

--- spec/models/email_request_spec.rb
+++ spec/models/email_request_spec.rb
@@ -3,19 +3,18 @@ require "rails_helper"
 describe EmailRequest, type: :model do
 
   describe "api" do
-    it { expect(EmailRequest).to respond_to :get_by_status_and_email_template_id  }
+    it { expect(EmailRequest).to respond_to :get_by_status_and_email_template_id }
+    it { expect(EmailRequest).to respond_to :available_status }
+    it { is_expected.to respond_to :template_name }
   end
 
   describe "validations" do
-
     it { should validate_presence_of :email_to }
     it { should validate_presence_of :email_template }
     it { should validate_presence_of :status }
-
   end
 
   describe "default values" do
-
     let :email_template do
       EmailTemplate.create(name: "NAME", subject: "SUBJECT",
                            body: "BODY")
@@ -38,4 +37,22 @@ describe EmailRequest, type: :model do
       expect(email_request).to_not be_nil
     end
   end
+
+  describe ".available_status" do
+    let(:status_list){ [["sending", 0], ["success", 1], ["failed", 2]] }
+    it "returns an array with status text and status id/value" do
+      expect(EmailRequest.available_status).to be == status_list
+    end
+  end
+
+  describe "#template_name" do
+    let(:email_template){build(:email_template, name: 'Custom Template')}
+    let(:request){build(:email_request, email_template: email_template)}
+
+    it "returns the value of email_template.name associated" do
+      expect(request.template_name).to be == 'Custom Template'
+    end
+
+  end
 end
+
