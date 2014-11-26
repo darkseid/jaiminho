6405edd428decab5271cb86e9736f23f739dcfce
Sergio Azevedo <sergior.jr@gmail.com>
2014-11-26 19:25:30 -0200

    introduces token based api security.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,6 +1,15 @@
 module Api
   class ApiController < ApplicationController
+    before_action :check_access
     respond_to :json
+
+    private
+
+    def check_access
+      authenticate_or_request_with_http_token do |token, options|
+        ApiKey.exists?(access_token: token)
+      end
+    end
 
   end
 end
--- spec/controllers/api/v1/email_requests_controller_spec.rb
+++ spec/controllers/api/v1/email_requests_controller_spec.rb
@@ -4,10 +4,14 @@ module Api
     describe EmailRequestsController, type: :controller do
 
       describe "POST create" do
-        context "with valid params" do
+        let(:valid_token){ ApiKey.create.access_token }
+        let(:token){ActionController::HttpAuthentication::Token.encode_credentials(valid_token)}
+
+        context "with valid params and valid token" do
           let(:email_request) { build :email_request }
 
           before do
+            request.headers['HTTP_AUTHORIZATION'] = token
             params = {
               "to" => "rcuba@estantevirtual.com.br",
               "template_id" => 1,
@@ -15,10 +19,6 @@ module Api
             }
             allow_any_instance_of(EmailRequest).to receive(:save).and_return true
             post :create, email_request: params, format: :json
-          end
-
-          it "returns http success" do
-            expect(response).to be_success
           end
 
           it "builds the email_request" do
@@ -30,13 +30,14 @@ module Api
           end
         end
 
-        context "with invalid params" do
+        context "with invalid params and valid token" do
           before do
             params = {
               "email_to" => nil,
               "template" => 1,
               "data" => {"name" => "Raphael Cuba"}
             }
+            request.headers['HTTP_AUTHORIZATION'] = token
             allow_any_instance_of(EmailRequest).to receive(:save).and_return false
             post :create, email_request: params, format: :json
           end
@@ -46,8 +47,9 @@ module Api
           end
         end
 
-        context "without params" do
+        context "with valid token but without params" do
           before do
+            request.headers['HTTP_AUTHORIZATION'] = token
             post :create, email_request: nil, format: :json
           end
 
@@ -55,8 +57,33 @@ module Api
             expect(response).to have_http_status 400
           end
         end
-      end
 
+        context "without access token" do
+          before do
+            post :create, email_request: nil, format: :json
+          end
+
+          it "return http status 401" do
+            expect(response).to have_http_status 401
+          end
+        end
+
+        context "with a invalid access token" do
+          let(:invalid_token) do
+            ActionController::HttpAuthentication::Token.encode_credentials("123")
+          end
+
+          before do
+            request.headers['HTTP_AUTHORIZATION'] = invalid_token
+            post :create, email_request: nil, format: :json
+          end
+
+          it "return http status 401" do
+            expect(response).to have_http_status 401
+          end
+        end
+
+      end
     end
   end
 end
