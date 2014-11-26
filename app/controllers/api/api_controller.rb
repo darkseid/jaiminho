13dda12143e83e96e16e5ef9ff9714c7120a69ae
Sergio Azevedo <sergior.jr@gmail.com>
2014-11-26 16:16:04 -0200

    removes ApiController#error

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -2,10 +2,5 @@ module Api
   class ApiController < ApplicationController
     respond_to :json
 
-    def error(code, message = nil)
-      @code = code
-      @message = message
-      render :error, status: code
-    end
   end
 end
